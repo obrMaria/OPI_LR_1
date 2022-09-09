84ce05fe335e4ea8bccd1379add765c7a959c77b
obrMaria <midsummer.madness@mail.ru>
2022-09-10 02:59:10 +0300

    1 commit

---
+++ folder_1/folder_1/folder_1.cpp
@@ -0,0 +1,17 @@
+﻿#include<stdio.h>
+#include<cstdlib>
+#include<Windows.h>
+#include<cmath>
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	
+	cout << (" начало работы ");
+
+	return 0;
+	system("pause");
+}

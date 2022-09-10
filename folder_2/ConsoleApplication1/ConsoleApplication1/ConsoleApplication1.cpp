b4c1098f07405f3210c1c1d46c4af1c8d69fd85b
obrMaria <midsummer.madness@mail.ru>
2022-09-10 03:16:38 +0300

    5 commit

---
+++ folder_2/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,17 @@
+﻿#include<stdio.h>
+#include<cstdlib>
+#include<Windows.h>
+#include<cmath>
+#include<iostream>
+
+usingnamespace std;
+
+int main()
+{
+	setlocale(0, "");
+	int n, k;
+	
+
+	return 0;
+	system("pause");
+}
